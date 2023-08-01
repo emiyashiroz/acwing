75987aa7216c0e55d9b704a7067ad2a35ae12495
zw <1018512223@qq.com>
2023-08-01 23:58:05 +0800

    feat: merge sort

--- alg_test.go
+++ alg_test.go
@@ -6,8 +6,13 @@ import (
 )
 
 func TestQuickSort(t *testing.T) {
+	//a := []int{782, 23, 2, 4, 123, 5, 12, 12, 41, 4}
+	//fmt.Println(a)
+	//QuickSort(a, 0, 9)
+	//fmt.Println(a)
+
 	a := []int{782, 23, 2, 4, 123, 5, 12, 12, 41, 4}
 	fmt.Println(a)
-	QuickSort(a, 0, 9)
+	MergeSort(a, 0, 9)
 	fmt.Println(a)
 }
--- base_alg.go
+++ base_alg.go
@@ -21,3 +21,35 @@ func QuickSort(q []int, l, r int) {
 	QuickSort(q, l, j)
 	QuickSort(q, j+1, r)
 }
+
+// MergeSort 归并排序
+func MergeSort(q []int, l, r int) {
+	if l >= r {
+		return
+	}
+	j := (l + r) >> 1
+	MergeSort(q, l, j)
+	MergeSort(q, j+1, r)
+	t := make([]int, r-l+1)
+	m, n, k := l, j+1, 0
+	for ; m <= j && n <= r; k++ {
+		if q[m] <= q[n] {
+			t[k] = q[m]
+			m++
+		} else {
+			t[k] = q[n]
+			n++
+		}
+	}
+	for ; m <= j; k++ {
+		t[k] = q[m]
+		m++
+	}
+	for ; n <= r; k++ {
+		t[k] = q[n]
+		n++
+	}
+	for i := l; i <= r; i++ {
+		q[i] = t[i-l]
+	}
+}
